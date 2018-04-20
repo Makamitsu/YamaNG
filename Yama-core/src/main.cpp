b33ac3bdc9d950aa40eb19674bbe320ef0347591
Augustin Ambiehl <ambiehlaugustin@gmail.com>
2018-04-21 00:07:41 +0200

    [FTC] Api Assimp ajout.

--- Yama-core/src/main.cpp
+++ Yama-core/src/main.cpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include "graphics\window.h"
 #include "graphics\Renderer.h"
 #include "graphics\VertexBuffer.h"
@@ -25,6 +29,15 @@ int main() {
 	GLCall(glEnable(GL_BLEND));
 	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
+
+
+	auto simpleMan = aiImportFile("res/models/simpleMan2.blend", aiProcessPreset_TargetRealtime_MaxQuality);
+
+	std::cout << "Meches found: " << simpleMan->mNumMeshes << "\n";
+	std::cout << "Vertices found: " << simpleMan->mMeshes[0]->mNumVertices << "\n" ;
+
+	auto vertices = simpleMan->mMeshes[0]->mVertices;
+
 	float positions[] = {
 		-0.5f, 0.5f,  0.0, 1.0,
 		 0.5f, 0.5f,  1.0, 1.0,
@@ -37,19 +50,20 @@ int main() {
 		2, 3, 0
 	};
 
+
 	VertexArray va;
-	VertexBuffer vb(positions, 16 * sizeof(float));
+	VertexBuffer vb(vertices, simpleMan->mMeshes[0]->mNumVertices * 3 * sizeof(float));
 
 	VertexBufferLayout layout;
-	layout.push<GL_FLOAT, sizeof(GLfloat)>(2);
-	layout.push<GL_FLOAT, sizeof(GLfloat)>(2);
+	layout.push<GL_FLOAT, sizeof(GLfloat)>(3);
+	//layout.push<GL_FLOAT, sizeof(GLfloat)>(2);
 	va.addBuffer(vb, layout);
 
-	IndexBuffer vi(indices, 6*sizeof(unsigned int));
+	IndexBuffer vi(indices, 6 * sizeof(unsigned int));
 
 
 
-	glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+	glm::mat4 proj = glm::ortho(-200.0f, 200.0f, -100.5f, 100.5f, -100.0f, 100.0f);
 	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f));
 
 	glm::mat4 mvp =  proj * view;
